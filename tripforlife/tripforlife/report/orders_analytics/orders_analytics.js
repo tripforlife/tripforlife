e56574b7dade791e1e00293af0e8246ade37e6fb
tripforlife <tripforlife.in.rishikesh@gmail.com>
2021-06-29 08:12:17 +0000

    initial commit

---
+++ tripforlife/tripforlife/report/orders_analytics/orders_analytics.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2016, Vamsi Krishna and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Orders Analytics"] = {
+	"filters": [
+
+	]
+};
