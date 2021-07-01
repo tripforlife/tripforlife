a3aeee1e9f6ab576af3b10998c3fccfe85f5196c
tripforlife <tripforlife.in.rishikesh@gmail.com>
2021-07-01 06:46:09 +0000

    second commit for website

---
+++ tripforlife/tripforlife/doctype/camp_activities/test_camp_activities.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, Vamsi Krishna and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestCampActivities(unittest.TestCase):
+	pass
--- tripforlife/tripforlife/doctype/camp_facilities/camp_facilities.py
+++ tripforlife/tripforlife/doctype/camp_facilities/camp_facilities.py
@@ -2,7 +2,7 @@
 # For license information, please see license.txt
 
 # import frappe
-from frappe.website.website_generator import WebsiteGenerator
+from frappe.model.document import Document
 
-class Camps(WebsiteGenerator):
+class CampFacilities(Document):
 	pass
