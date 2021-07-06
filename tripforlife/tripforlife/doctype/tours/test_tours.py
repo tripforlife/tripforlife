6a62ea13d8241f4dd799062cc2dc8b8fc94470fc
tripforlife <tripforlife.in.rishikesh@gmail.com>
2021-07-06 16:53:10 +0000

    tour items added

---
+++ tripforlife/tripforlife/doctype/tours/test_tours.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, Vamsi Krishna and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestTours(unittest.TestCase):
+	pass
---
+++ tripforlife/tripforlife/doctype/tours/tours.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Vamsi Krishna and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Tours', {
+	// refresh: function(frm) {
+
+	// }
+});
